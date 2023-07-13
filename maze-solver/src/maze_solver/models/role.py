dce7b4e93c45fac5f2928c2309077d2d5e1adbc0
louwJohan <louwjohanc@gmail.com>
2023-07-13 09:23:36 +0100

    First commit

---
+++ maze-solver/src/maze_solver/models/role.py
@@ -0,0 +1,10 @@
+from enum import IntEnum, auto
+
+class Role(IntEnum):
+    NONE = 0
+    ENEMY = auto()
+    ENTRANCE = auto()
+    EXIT = auto()
+    EXTERIOR = auto()
+    REWARD = auto()
+    WALL = auto()
